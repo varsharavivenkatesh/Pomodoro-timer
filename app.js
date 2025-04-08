ed4ebd2bc4c41649f16a2ba1c3e919d81c159616
varsharavivenkatesh <varsharaviven@gmail.com>
2025-04-07 21:29:51 -0500

    put in folder
    put it in folder

---
+++ app.js
@@ -0,0 +1,35 @@
+const start=document.getElementById("start");
+const stop=document.getElementById("Stop");
+const reset=document.getElementById("reset");
+const timer=document.getElementById("timer")
+
+let timeLeft=1500; /*25 minutes in seconds*/
+let interval;
+
+const updateTimer=()=>{
+    const minutes=Math.floor(timeLeft/60);
+    const seconds=timeLeft%60;
+    timer.innerHTML=`${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`;
+};
+const startTimer=() => {
+    interval=setInterval(()=>{timeLeft--;updateTimer();
+     if(timeLeft==0){
+        clearInterval(interval);
+        alert("Time's up!");
+        timeleft=1500;
+        updateTimer();
+     }
+    },1000);
+};
+const stopTimer=() => clearInterval(interval);
+const resetTimer=()=>{
+    clearInterval(interval);
+    timeLeft=1500;
+    updateTimer();
+};
+start.addEventListener("click",startTimer);
+Stop.addEventListener("click",stopTimer);
+reset.addEventListener("click",resetTimer);
+
+
+
